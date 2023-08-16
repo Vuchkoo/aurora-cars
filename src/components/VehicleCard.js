d8af6c60819e698e194b7c8dc794441c009e8eed
Vuchkoo <filipvuk97@gmail.com>
2023-08-16 18:24:20 +0200

    feat: added delete vehicle functionality

--- src/components/VehicleCard.js
+++ src/components/VehicleCard.js
@@ -1,4 +1,6 @@
+import { supabase } from "@/common/config/Supabase";
 import {
+  ActionIcon,
   Badge,
   Button,
   Card,
@@ -10,97 +12,27 @@ import {
   createStyles,
   rem,
 } from "@mantine/core";
-import {
-  IconGasStation,
-  IconGauge,
-  IconManualGearbox,
-  IconUsers,
-} from "@tabler/icons-react";
+import { IconX } from "@tabler/icons-react";
 import React from "react";
 
-function VehicleCard() {
-  const vehicleData = [
-    { label: "4 passengers", icon: IconUsers },
-    { label: "100 km/h in 4 seconds", icon: IconGauge },
-    { label: "Automatic gearbox", icon: IconManualGearbox },
-    { label: "Electric", icon: IconGasStation },
-  ];
-
-  const useStyles = createStyles((theme) => ({
-    icon: {
-      marginRight: rem(5),
-    },
-    section: {
-      padding: theme.spacing.md,
-      borderTop: `${rem(1)} solid dark`,
-    },
-    label: {
-      marginBottom: theme.spacing.xs,
-      lineHeight: 1,
-      fontWeight: 700,
-      fontSize: theme.fontSizes.xs,
-      letterSpacing: rem(-0.25),
-      textTransform: "uppercase",
-    },
-  }));
-
-  //   const useStyles = createStyles((theme) => ({
-  //     card: {
-  //       backgroundColor:
-  //         theme.colorScheme === "dark" ? theme.colors.dark[7] : theme.white,
-  //     },
-
-  //     imageSection: {
-  //       padding: theme.spacing.md,
-  //       display: "flex",
-  //       alignItems: "center",
-  //       justifyContent: "center",
-  //       borderBottom: `${rem(1)} solid ${
-  //         theme.colorScheme === "dark"
-  //           ? theme.colors.dark[4]
-  //           : theme.colors.gray[3]
-  //       }`,
-  //     },
-
-  //     label: {
-  //       marginBottom: theme.spacing.xs,
-  //       lineHeight: 1,
-  //       fontWeight: 700,
-  //       fontSize: theme.fontSizes.xs,
-  //       letterSpacing: rem(-0.25),
-  //       textTransform: "uppercase",
-  //     },
-
-  //     section: {
-  //       padding: theme.spacing.md,
-  //       borderTop: `${rem(1)} solid ${
-  //         theme.colorScheme === "dark"
-  //           ? theme.colors.dark[4]
-  //           : theme.colors.gray[3]
-  //       }`,
-  //     },
-
-  //     icon: {
-  //       marginRight: rem(5),
-  //       color:
-  //         theme.colorScheme === "dark"
-  //           ? theme.colors.dark[2]
-  //           : theme.colors.gray[5],
-  //     },
-  //   }));
-
-  const { classes } = useStyles();
-  const features = vehicleData.map((feature) => (
-    <Center key={feature.label}>
-      <feature.icon size="1.05rem" className={classes.icon} stroke={1.5} />
-      <Text size="xs">{feature.label}</Text>
-    </Center>
-  ));
+function VehicleCard({ data, model, index }) {
+  const removeVehicle = async (index, id) => {
+    // console.log(index, id);
+    const { data } = await supabase.from("vehicle_make").delete().eq("id", id);
+    if (data) {
+      console.log(index, id);
+    }
+  };
 
   return (
     <>
       <Card withBorder radius="md" w={300}>
         <Card.Section withBorder>
+          <Flex justify="flex-end">
+            <ActionIcon onClick={() => removeVehicle(index, data.id)}>
+              <IconX />
+            </ActionIcon>
+          </Flex>
           <Center>
             <Image
               src="https://i.imgur.com/ZL52Q2D.png"
@@ -112,41 +44,20 @@ function VehicleCard() {
           </Center>
         </Card.Section>
 
-        <Group position="apart" mt="md">
-          <div>
-            <Text fw={500}>Tesla Model S</Text>
-            <Text fz="xs" c="dimmed">
-              Free recharge at any station
+        <Card.Section>
+          <Center mt="md">
+            <Text fw={500}>
+              {data?.name} <span>{model?.name}</span>
             </Text>
-          </div>
-          <Badge variant="outline" color="violet">
-            25% off
-          </Badge>
-        </Group>
-
-        <Card.Section mt="md" className={classes.section} withBorder>
-          <Text fz="sm" c="dimmed" className={classes.label}>
-            Basic configuration
-          </Text>
-          <Group spacing={10} mb={10}>
-            {features}
-          </Group>
+          </Center>
         </Card.Section>
 
-        <Card.Section className={classes.section}>
-          <Group spacing={30}>
-            <div>
-              <Text fz="xl" fw={700} sx={{ lineHeight: 1 }}>
-                $168.00
-              </Text>
-              <Text fz="sm" c="dimmed" fw={500} sx={{ lineHeight: 1 }} mt={3}>
-                per day
-              </Text>
-            </div>
-            <Button radius="xl" color="violet" style={{ flex: 1 }}>
-              Rent now
+        <Card.Section>
+          <Center my={20}>
+            <Button variant="outline" color="violet" size="xs">
+              Edit
             </Button>
-          </Group>
+          </Center>
         </Card.Section>
       </Card>
     </>
