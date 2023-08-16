da0b92e3f2c09feff305fa97fbd1389cd6963074
Vuchkoo <filipvuk97@gmail.com>
2023-08-16 18:05:43 +0200

    feat: forms changes and create functionality

--- src/components/forms/CreateVehicle.js
+++ src/components/forms/CreateVehicle.js
@@ -1,46 +1,142 @@
-import { Button, Center, TextInput } from "@mantine/core";
+import { supabase } from "@/common/config/Supabase";
+import {
+  Button,
+  Center,
+  Flex,
+  Group,
+  SimpleGrid,
+  Stepper,
+  TextInput,
+} from "@mantine/core";
 import { useForm } from "@mantine/form";
-import React from "react";
+import React, { useState } from "react";
 
-function CreateVehicle() {
-  const vehicleForm = useForm({
+function CreateVehicle({ close }) {
+  const [activeStep, setActiveStep] = useState(0);
+
+  const vehicleMakeForm = useForm({
     initialValues: {
+      id: undefined,
       name: "",
-      abrv: "",
-      model: "",
+      abbrv: "",
     },
   });
 
+  const vehicleModelForm = useForm({
+    initialValues: {
+      make_id: vehicleMakeForm.values.id,
+      name: "",
+      abbrv: "",
+    },
+  });
+
+  const nextStep = async () => {
+    if (activeStep === 0) {
+      const { data, error } = await supabase
+        .from("vehicle_make")
+        .upsert({ ...vehicleMakeForm.values })
+        .select()
+        .single();
+      vehicleModelForm.setFieldValue("make_id", data.id);
+      if (error) {
+        console.log("Error");
+      } else console.log(data);
+    }
+    setActiveStep((current) => (current < 2 ? current + 1 : current));
+  };
+
+  const prevStep = () =>
+    setActiveStep((current) => (current > 0 ? current - 1 : current));
+
+  const createNewModel = async () => {
+    const { data, error } = await supabase
+      .from("vehicle_model")
+      .upsert({ ...vehicleModelForm.values });
+    if (error) {
+      console.log("Error");
+    } else console.log(data);
+  };
+
+  console.log(vehicleMakeForm.values);
+  console.log(vehicleModelForm.values);
+
   return (
     <>
+      <Stepper
+        active={activeStep}
+        onStepClick={setActiveStep}
+        breakpoint="sm"
+        color="violet"
+      >
+        <Stepper.Step label="Vehicle Name"></Stepper.Step>
+        <Stepper.Step label="Vehicle Model"></Stepper.Step>
+      </Stepper>
+
       <form>
-        <TextInput
-          mt="md"
-          label="Name"
-          placeholder="e.g. Volkswagen"
-          withAsterisk
-          //   {...vehicleForm.getInputProps("name")}
-        />
+        {activeStep === 0 ? (
+          <>
+            <TextInput
+              mt="md"
+              label="Name"
+              placeholder="e.g. Volkswagen"
+              withAsterisk
+              {...vehicleMakeForm.getInputProps("name")}
+            />
 
-        <TextInput
-          mt="md"
-          label="Abrv"
-          placeholder="e.g. VW"
-          //   {...vehicleForm.getInputProps("abrv")}
-        />
+            <TextInput
+              mt="md"
+              label="Abbrv"
+              placeholder="e.g. VW"
+              {...vehicleMakeForm.getInputProps("abbrv")}
+            />
+          </>
+        ) : (
+          activeStep === 1 && (
+            <>
+              <TextInput
+                mt="md"
+                label="Model"
+                placeholder="e.g. Golf"
+                withAsterisk
+                {...vehicleModelForm.getInputProps("name")}
+              />
 
-        <TextInput
-          mt="md"
-          label="Model"
-          placeholder="e.g. Golf"
-          withAsterisk
-          //   {...vehicleForm.getInputProps("model")}
-        />
-
-        <Center mt={40}>
-          <Button color="violet">Create</Button>
-        </Center>
+              <TextInput
+                mt="md"
+                label="Abbrv"
+                placeholder="e.g. GTI"
+                {...vehicleModelForm.getInputProps("abbrv")}
+              />
+            </>
+          )
+        )}
       </form>
+
+      <Group position="center" spacing={20} mt={20}>
+        {/* <Button
+          onClick={prevStep}
+          display={activeStep === 0 && "none"}
+          variant="outline"
+          radius={50}
+          color="violet"
+        >
+          Back
+        </Button> */}
+        <Button
+          onClick={() => {
+            if (activeStep === 0) {
+              nextStep();
+            } else if (activeStep === 1) {
+              createNewModel();
+              close();
+            }
+          }}
+          radius={50}
+          color="violet"
+        >
+          {activeStep < 1 ? "Next" : "Submit"}
+        </Button>
+      </Group>
     </>
   );
 }
